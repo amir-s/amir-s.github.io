ca0bb78a362963872b6bc019f0104b429d4d69fd
Amir <amir.saboury@gmail.com>
2013-11-02 00:26:32 +0330

    initial

---
+++ blog.js
@@ -0,0 +1,21 @@
+{
+    "title": "Amir Saboury",
+    "pageTitle": "Amir Saboury's personal blog",
+    "description": "sharing ideas about computer science, software eng. and geeky stuff!",
+    "url": "http://amir-s.github.io/",
+    "author": {
+        "name": {
+            "first": "Amir",
+            "last": "Saboury"
+        },
+        "email": "amir.s@acm.org",
+        "twitter": "as32",
+        "facebook": "amir.sa"
+    },
+    "pageSize": 3,
+    "theme": {
+        "index": "./theme/index.html",
+        "tags": "./theme/tags.html",
+        "post": "./theme/post.html"
+    }
+}
